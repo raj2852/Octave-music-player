4f2bcff48164a9e6aa11adf5130204edf7ed700f
Sayantan Bhattacharya <sayantanofficialmail@gmail.com>
2021-10-01 01:11:51 +0530

    Update Music_Player.js

--- assets/js/Music_Player.js
+++ assets/js/Music_Player.js
@@ -13,18 +13,19 @@ Songs=[{Song name:Stereo Love.mp3
         {Song name:Attention.mp3
         Album name:AtttentionA.jpg
         Artist name:AttentionB.jpg},
-        Songs=[{Song name:Stereo Love.mp3
-        Album name:Stereo LoveA.jpg
-        Artist name:Stereo LoveB.jpg},
-        {Song name:Far From Love .mp3
-        Album name:Far From LoveA.jpg
-        Artist name:Far From LoveB.jpg},
-        {Song name:Sugar And Brownies.mp3
-        Album name:Sugar And BrowniesA.jpg
-        Artist name:Sugar And BrowniesB.jpg},
-        {Song name:See You Again.mp3
-        Album name:See you againA.jpg
-        Artist name:See you againB.jpg},
-        {Song name:Attention.mp3
-        Album name:AtttentionA.jpg
-        Artist name:AttentionB.jpg},
+        {Song name:Raat Di Gedi.mp3
+        Album name:Raat Di GediA.jpg
+        Artist name:Raat Di GediB.jpg},
+        {Song name:Moner Manus.mp3
+        Album name:Moner ManusA.jpg
+        Artist name:Moner ManusB.jpg},
+        {Song name:Dil Hum Hum.mp3
+        Album name:Dil Hum HumA.jpg
+        Artist name:Dil Hum HumB.jpg},
+        {Song name:Aahun Aahun.mp3
+        Album name:Aahun AahunA.jpg
+        Artist name:Aahun AahunB.jpg},
+        {Song name:Let The Music Play.mp3
+        Album name:Let The Music PlayA.jpg
+        Artist name:Let The Music PlayB.jpg}]
+        
